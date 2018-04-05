5fa1c706ae615838558e9a86a32f0c1a6d1f646f
musfar <musfar@qburst.com>
2018-04-05 17:43:28 +0530

    redirect cmmnt added

--- src/actions/userLogin.js
+++ src/actions/userLogin.js
@@ -25,13 +25,13 @@ export const fetchUserToken = (googleResponse, showInfo, routeToHome, alexaParam
         isAlexa
       })
     .then((response) => {
-      console.log(response);
       if (response.data.status === 200 || isAlexa) {
         if (isAlexa) {
-          window.location = decodeURIComponent(`${alexaParams.redirect_uri}` +
-          `#access_token=${response.data.token}` +
-          `&state=${alexaParams.state}` +
-          `&token_type=BearerToken`);
+          // window.location = decodeURIComponent(`${alexaParams.redirect_uri}` +
+          // `#access_token=${response.data.token}` +
+          // `&state=${alexaParams.state}` +
+          // `&token_type=BearerToken`);
+          console.log(response);
         }
         else {
           const userDetails = {
